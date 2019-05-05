2bd143e6f037f70fa5953a2852dd576db4e55071
Alex76 <alex0676@gmail.com>
2019-05-05 23:12:16 +0300

    edit file

--- src/components/VHome.vue
+++ src/components/VHome.vue
@@ -2,10 +2,7 @@
   <div>
     <h3>Welcome!</h3>
     <p>You'll probably want to remove all of this stuff, but hey it at least it works!</p>
-    <p>
-      ~
-      <a href="https://evanagee.com/">Evan Agee</a>
-    </p>
+    <p><a href="https://evanagee.com/">Evan Agee</a></p>
 
     <recent-posts-widget limit="5">Recent Posts</recent-posts-widget>
     <pages-widget limit="5">Pages</pages-widget>
