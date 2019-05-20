4d8b2bbbd9fe94f66ff36b104b9506fdccd7a3e0
Alex <alex0676@gmail.com>
2019-05-20 07:22:55 +0300

    edit files

--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,7 @@ export default {
   LOADING_SEGMENTS: 2,
   THEME_DIR_PATH: "/wp-content/themes/_customthema/dist/",
   API_BASE_PATH: "/wp-json/wp/v2/",
-  URL_RESOURSE  : "http://"+window.location.hostname+"",
+  URL_RESOURSE  : ""+document.location.origin+"",
   WC : {
       CONSUMERKEY   : "ck_07da79da932bba6211d0b8dc58e791b1ffcd123a",
       CONSUMERSECRET: "cs_fbc1e36ee6d7c05e373fbce09d65adeed25a31e7",
