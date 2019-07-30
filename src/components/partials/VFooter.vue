6ada4df4c4f9053999875772d3c57b20182425d6
Alex <y.mih0676@gmail.com>
2019-07-30 15:17:19 +0300

    fix files

--- src/components/partials/VFooter.vue
+++ src/components/partials/VFooter.vue
@@ -1,3 +1,17 @@
 <template>
-  <footer class="site-footer">This is the footer.</footer>
+  <footer class="site-footer">
+    <section class="uk-container">
+      <div class="uk-child-width-1-3 uk-text-left uk-text-small" uk-grid>
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make</div>
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make</div>
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make</div>
+      </div>
+    </section>
+  </footer>
 </template>
